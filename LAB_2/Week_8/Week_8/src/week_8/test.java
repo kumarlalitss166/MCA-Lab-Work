8dcafcad7e839ab8e068ab593dbd326c8ef4539f
Lalit Kumar <kumarlalitss166@gmail.com>
2025-04-21 23:04:14 +0530

    Week_8 DONE

--- LAB_2/Week_8/Week_8/src/week_8/test.java
+++ LAB_2/Week_8/Week_8/src/week_8/test.java
@@ -1,9 +1,9 @@
-
 package week_8;
 public class test {
     public static void main(String[] args) {
         try {
             int a = 5 / 0;
+            System.out.println(a);
         } catch (ArithmeticException e) {
             System.out.println("Division by zero error.");
         } catch (NullPointerException e) {
